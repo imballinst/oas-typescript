71941dbf2b6c983c340e5a4a71c2ca2c9ea1072f
Try Ajitiono <ballinst@gmail.com>
2023-07-20 12:59:39 +0000

    add example oas-swagger-ui

--- packages/oas-swagger-ui/src/library/plugin-overrides/types.ts
+++ packages/oas-swagger-ui/src/library/plugin-overrides/types.ts
@@ -1,10 +1,10 @@
 export interface OasSwaggerUiSecurityConfig {
   badgesField?: string;
-  badgesDefaultValue?: Array<{ label: string; value: string }>;
+  badgesDefaultValue?: Array<{ label: string; value?: string }>;
   badgesProcessFn?: (
     securityKey: string,
     value?: string[]
-  ) => Array<{ label: string; value: string }>;
+  ) => Array<{ label: string; value?: string }>;
 }
 
 export interface OasSwaggerUiConfig {
