6d8fe2378080365f3b1845af7cbc1456c73f5e15
Try Ajitiono <ballinst@gmail.com>
2023-11-11 02:32:17 +0000

    see status in CI

--- docs/scripts/validate-sync-api.mjs
+++ docs/scripts/validate-sync-api.mjs
@@ -3,6 +3,7 @@ import { execSync } from 'child_process';
 const output = execSync('git status -s').toString().trim();
 if (output) {
   throw new Error(
-    'There are differences after running the `yarn sync-api-reference` in the `docs` folder. You might want to run it first, then re-commit.`'
+    'There are differences after running the `yarn sync-api-reference` in the `docs` folder. You might want to run it first, then re-commit.` Changes include: ' +
+      output
   );
 }
